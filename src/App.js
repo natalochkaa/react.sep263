eaac8a3a482bfed822d203efe3c4ad76f090fbad
natalochkaa <natalochka356@gmail.com>
2022-01-31 02:02:20 +0200

    cw1

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
-import React from 'react';
+import './App.css';
+import {useEffect, useState} from "react";
+
 
 const App = () => {
-  return (
-      <div>
-        
-      </div>
-  );
+    let [flight, setFlight] = useState([]);
+
+    useEffect(() => {
+        fetch("https://api.spacexdata.com/v3/launches/")
+            .then(value => value.json())
+            .then(value => {
+                let filter = value.filter(flight => flight.launch_year !== "2020");
+                setFlight(filter);
+            })
+    },[]);
+    return (
+        <div className={`Card`}>
+            <div className={`spaceX`}> SpaceX </div>
+            {
+                flight.map(value => <div className={`styleCard`} key ={value.flight_number}>
+                    <img src={value.links.mission_patch_small} alt=""/>
+                    <h1 className={`name`}>{value.mission_name}</h1>
+                    <br/>
+                    {value.launch_year}
+                </div>)
+            }
+        </div>
+    );
 };
 
 export default App;
